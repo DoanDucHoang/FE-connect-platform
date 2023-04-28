11a73fe9e706a79d5604ed6883ebf6dc983d78dc
DoanDucHoang <doanduckien.2001@gmail.com>
2023-04-28 16:52:37 +0700

    update FE

--- src/server/routes/member.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { updateProfile } from '../controllers/member.js';
-
-const router = express.Router();
-
-router.post('/updateprofile', updateProfile);
-
-export default router;
